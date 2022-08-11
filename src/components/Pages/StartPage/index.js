ec5de99e445f0b7926c875a82ad7cef418556915
mchuduk <mihail2014mihail@gmail.com>
2022-08-11 17:18:46 +0300

    Update SignUp form

--- src/components/Pages/StartPage/index.js
+++ src/components/Pages/StartPage/index.js
@@ -1,18 +1,28 @@
-import MonetizationOnRoundedIcon from '@mui/icons-material/MonetizationOnRounded';
-import SignUpForm from '../../Forms/SignUpForm';
-import styles from './StartPage.module.scss';
+import { TextField } from "@mui/material";
+import MonetizationOnRoundedIcon from "@mui/icons-material/MonetizationOnRounded";
+import styles from "./StartPage.module.scss";
 
 function StartPage() {
-    console.log(styles.monetizationOnRoundedIcon);
+  console.log(styles.monetizationOnRoundedIcon);
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.logo}>
-        <MonetizationOnRoundedIcon sx={{ fontSize: '60px', color: '#232C47' }} />
-        <h3>fintracker</h3>
-      </div>
-      <div>
-        <SignUpForm />
+      <div className={styles.box}>
+        <div className={styles.header}>
+          <MonetizationOnRoundedIcon sx={{ fontSize: "50px" }} />
+          <p>Sign up to fintracker</p>
+        </div>
+        <div className={styles.content}>
+          <TextField label="Email" variant="standard" />
+          <TextField
+            label="Password"
+            variant="standard"
+            inputProps={{
+              type: "password",
+              autoComplete: "new-password",
+            }}
+          />
+        </div>
       </div>
     </div>
   );
