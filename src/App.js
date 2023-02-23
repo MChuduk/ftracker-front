9100e19c4a82dd3dbd7df2205a0bd13688646155
Mchuduk <mihail2014mihail@gmail.com>
2023-02-23 18:31:55 +0300

    Add PrivateRoute component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import SignUpForm from "./components/Forms/SignUpForm";
 import { Routes, Route, Link } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
@@ -7,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SignUpPage/>} />
+        <Route path="/" element={<SignInPage/>} />
         <Route path="/signUp" element={<SignUpPage/>} />
         <Route path="/signIn" element={<SignInPage/>} />
       </Routes>
---
+++ src/components/PrivateRoute/index.js
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+function PrivateRoute() {
+    const isAuth = true;
+
+    return isAuth ? < Outlet /> : <Navigate to="/signIn" />
+}
+
+export default PrivateRoute;
